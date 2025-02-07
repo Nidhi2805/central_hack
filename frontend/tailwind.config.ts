ef413bd39c8247adacf3b970013bf43c39baa4c3
har-sat <harsat.ponnusamy@gmail.com>
2025-02-07 23:22:13 +0530

    chore: update brand colors in tailwindconfig

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,8 +9,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        text: "#EBE9FC",
+        background: "#040404",
+        "background-secondary": "#131313",
+        neutral: "#2F2F2F",
+        accent: "#0600C2",
+        red: "#791F1F",
+        "red-secondary": "#150E0E",
+        green: "#21791F",
+        "green-secondary": "#11140D",
+        "buttons-primary": "#3A31D8",
+        "buttons-secondary": "#252342",
       },
     },
   },
