84706849d865f0291eb0f073ae8ef491a153f655
har-sat <harsat.ponnusamy@gmail.com>
2025-02-08 14:29:13 +0530

    action button

--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Navbar from "../../components/navbar";
 import Image from "next/image";
+import AccentButton from "@/components/accent-button";
 
-const Detection = () => {
+const DetectionPage = () => {
   const [selected, setSelected] = useState("text");
   const options = ["text", "image", "video", "audio"];
 
@@ -15,19 +16,18 @@ const Detection = () => {
   };
 
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
-      <Navbar />
+    <div className="min-h-screen bg-black text-text flex flex-col items-center justify-center">
       <h2 className="text-3xl font-semibold mb-6 text-center">
         See Through the Fake—Detect Deepfakes Here.
       </h2>
-      <div className="flex space-x-4 mt-6 bg-gray-800 p-2 rounded-full">
+      <div className="flex space-x-4 mt-6 bg-neutral p-2 rounded-full">
         {options.map((option) => (
           <button
             key={option}
-            className={`px-6 py-3 rounded-full text-white text-lg transition-colors duration-200 ${
+            className={`px-6 py-3 rounded-full text-text text-lg transition-colors duration-200 ${
               selected === option
-                ? "bg-[#3A31D8]"
-                : "bg-gray-700 hover:bg-[#252342]"
+                ? "bg-buttons-primary"
+                : "hover:bg-buttons-secondary"
             }`}
             onClick={() => setSelected(option)}
           >
@@ -35,25 +35,27 @@ const Detection = () => {
           </button>
         ))}
       </div>
-      <label className="mt-8 w-[500px] h-32 flex items-center justify-center bg-black border border-gray-600 rounded p-3 text-white text-lg text-center cursor-pointer">
-        {selected === "text" ? (
-          <textarea
-            className="w-full h-full bg-transparent text-white text-lg resize-none outline-none"
-            placeholder="Enter text here"
-          />
-        ) : (
-          <div className="flex flex-col items-center">
-            <Image src="/upload.svg" alt="Upload" width={50} height={50} />
-            <p className="mt-2 text-white">Click here to upload</p>
-          </div>
-        )}
-        <input type="file" className="hidden" onChange={handleFileUpload} />
-      </label>
-      <button className="mt-8 bg-[#0600C2] text-white px-8 py-3 rounded text-xl hover:opacity-80">
+      <div>
+        <label className="mt-8 w-[500px] h-32 flex items-center justify-center bg-black border border-gray-600 rounded p-3 text-text text-lg text-center cursor-pointer">
+          {selected === "text" ? (
+            <textarea
+              className="w-full h-full bg-transparent text-text text-lg resize-none outline-none"
+              placeholder="Enter text here"
+            />
+          ) : (
+            <div className="flex flex-col items-center">
+              <Image src="/upload.svg" alt="Upload" width={50} height={50} />
+              <p className="mt-2 text-text">Click here to upload</p>
+            </div>
+          )}
+        </label>
+      </div>
+      <input type="file" className="hidden" onChange={handleFileUpload} />
+      <AccentButton className="mt-8">
         Detect
-      </button>
+      </AccentButton>
     </div>
   );
 };
 
-export default Detection;
+export default DetectionPage;
--- frontend/src/components/accent-button.tsx
+++ frontend/src/components/accent-button.tsx
@@ -1,12 +1,18 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-interface AccentButtonProps {
+interface AccentButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
 }
 
-export default function AccentButton({ children }: AccentButtonProps) {
+export default function AccentButton({
+  children,
+  ...props
+}: AccentButtonProps) {
   return (
-    <button className="text-text bg-buttons-primary px-6 py-3 rounded-xl w-fit font-bold text-2xl">
+    <button
+      {...props}
+      className={`text-text bg-buttons-primary px-6 py-3 rounded-xl w-fit font-bold text-2xl transition-colors duration-100 hover:bg-buttons-secondary ${props.className}`}
+    >
       {children}
     </button>
   );
