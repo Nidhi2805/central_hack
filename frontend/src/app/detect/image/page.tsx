d39631992178111c363726d7d6c8dd6ada624c20
har-sat <harsat.ponnusamy@gmail.com>
2025-02-08 14:30:57 +0530

    remove props in detect image page

--- frontend/src/app/detect/image/page.tsx
+++ frontend/src/app/detect/image/page.tsx
@@ -1,14 +1,14 @@
 import AccentButton from "@/components/accent-button";
-import { StaticImageData } from "next/image";
+// import { StaticImageData } from "next/image";
 import Image from "next/image";
 
-interface DetectImagePageProps {
-  image: StaticImageData;
-  outputImage: StaticImageData;
-  alt?: string;
-}
+// interface DetectImagePageProps {
+//   image: StaticImageData;
+//   outputImage: StaticImageData;
+//   alt?: string;
+// }
 
-const DetectImagePage: React.FC<DetectImagePageProps> = ({}) => {
+const DetectImagePage: React.FC = () => {
   return (
     <div className="flex flex-col items-center gap-16 justify-center h-[80vh]">
       <div className="bg-background-secondary rounded-[36px] flex flex-row w-fit px-6 py-12 gap-11 border-2 border-neutral">
